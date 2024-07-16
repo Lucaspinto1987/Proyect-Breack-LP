6e9acc116e35c6b17f38baf90b1bdf97c9eb5930
Lucas Pinto <lucasdamianpinto1987@gmail.com>
2024-07-16 18:13:23 +0200

    Finalizo el ejercicio del reloj digital

--- js/relojDigital.js
+++ js/relojDigital.js
@@ -1,14 +1,11 @@
-function hourDate() {
-    const now = new Date();
-    const formatDate = now.toLocaleDateString('es-ES');
-    const formatTime = now.toLocaleTimeString('es-ES');
+function hourDate() { //defino esta funcion para que se ejecute cada vez que sea llamada.
+    const timeCurrentDay = new Date(); // con new Date , creamos un nuevo date y este contiene fecha y hora y lo guardamos en la constante que creamos( Date es una palabra reservada que utilizamos para trabajar con fecha y hora )
+    const formatDate = timeCurrentDay.toLocaleDateString('es-ES'); // convertimos la fecha actual en un formato string usando la hora local de España
+    const formatTime = timeCurrentDay.toLocaleTimeString('es-ES'); // convertimos la hora actual en un formato string usando la hora local de España
 
-    document.getElementById('date').textContent = formatDate;
-    document.getElementById('time').textContent = formatTime;
+    document.getElementById('date').textContent = formatDate; //buscamos el elemento en el Html y actualizamos el contenido del DOM.
+    document.getElementById('time').textContent = formatTime; // idem
 }
 
-// Actualizar la fecha y hora cada segundo
-setInterval(hourDate, 1000);
-
-// Ejecutar la función inmediatamente para mostrar la fecha y hora al cargar la página
-hourDate();
+setInterval(hourDate, 1000); // con esto, actualizamos la fecha y hora a cada segundo.
+hourDate(); // ejecutamos la funcion para que se muestre en pantalla.
